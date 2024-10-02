cc762c99e7005f9d90b50b63eb0244778065c607
Rashed K <89036140+rashedkvm@users.noreply.github.com>
2024-10-01 22:18:22 -0400

    chore: file handler (#5)
    Signed-off-by: Rashed Kamal <krashed@vmware.com>

--- README.md
+++ README.md
@@ -1,4 +1,32 @@
 # bct-service
-BCT Service
+BCT Service examples
 
+## APP_PORT 
+Default to 8080
+
+## heatlhz endpoint
+
+Request: 
+```console
 curl bct-service.bct-service.svc.cluster.local:8080/api/healthz
+```
+
+Response: 
+```console
+{"etag":"8a906761-6ce4-4f7a-a981-6100dd6b93d0","status":"ok:","time":"2024-10-01T21:25:45.031615-04:00"}
+```
+
+## upload endpoint (uploads a file to http server)
+
+Request: 
+```http request
+ curl -X POST 0.0.0.0:8080/api/upload -H "Content-Type: multipart/form-data" -F "file=@/Users/johnny-goat/temp/values.yml"
+```
+
+Response:
+````http respone
+{"etag":"d41ee712-4573-410c-bdc2-30bb80c6fd4b","file":"'/tmp/values.yml' uploaded!","size":4225,"status":"ok","time":"2024-10-01T21:42:39.303881-04:00"}
+````
+
+
+ 
---
+++ cmd/main.go
@@ -0,0 +1,96 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/gin-contrib/requestid"
+	"github.com/gin-gonic/gin"
+)
+
+const (
+	appPort = "8080"
+	dst     = "/tmp"
+)
+
+func main() {
+	port := os.Getenv("APP_PORT")
+	if port == "" {
+		port = appPort
+	}
+
+	svc := setupRouter()
+
+	if err := svc.Run(fmt.Sprintf(":%v", port)); err != nil {
+		log.Fatal(err)
+	}
+}
+
+func setupRouter() *gin.Engine {
+	router := gin.Default()
+	router.Use(requestid.New())
+
+	// API routes and handlers
+	api := router.Group("/api")
+	{
+		api.GET("/healthz", healthz)
+		api.POST("/ping", PongHandler)
+		api.POST("/upload", uploadReportHandler)
+	}
+
+	return router
+}
+
+func healthz(c *gin.Context) {
+
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok:",
+		"etag":   requestid.Get(c),
+		"time":   time.Now(),
+	})
+}
+
+func PongHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	c.JSON(http.StatusOK, gin.H{
+		"message": "pong pong",
+	})
+}
+
+func uploadReportHandler(c *gin.Context) {
+	c.Header("Content-Type", "application/json")
+	// single file
+	file, _ := c.FormFile("file")
+	if file == nil {
+		c.JSON(http.StatusBadRequest, gin.H{
+			"status":  "error",
+			"message": "invalid request body",
+		})
+		return
+	}
+	log.Println(file.Filename)
+
+	destination := dst + "/" + file.Filename
+
+	// Upload the file to specific dst.
+	if err := c.SaveUploadedFile(file, destination); err != nil {
+		c.JSON(http.StatusInternalServerError, gin.H{
+			"status":  "error",
+			"message": err.Error(),
+		})
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"status": "ok",
+		"etag":   requestid.Get(c),
+		"time":   time.Now(),
+		"size":   file.Size,
+		"file":   fmt.Sprintf("'%s' uploaded!", destination),
+	})
+
+}
